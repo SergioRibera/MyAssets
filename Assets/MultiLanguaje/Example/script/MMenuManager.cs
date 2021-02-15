5ce45fc8fd037988d647df43f773a8b25fc75adf
Sergio Alejandro Ribera Costa <sergioalejandroriberacosta@gmail.com>
2021-02-15 03:34:15 -0400

    Add Example for MultiLanguaje

---
+++ Assets/MultiLanguaje/Example/script/MMenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MMenuManager : MonoBehaviour {
+
+    [SerializeField] string languajeSelected = "en";
+    [SerializeField] ScriptableLanguaje dbLanguaje;
+    [SerializeField] Dropdown languajeSelect;
+    [SerializeField] List<TextForChange> texts;
+
+    void Start(){
+        languajeSelect.onValueChanged.AddListener((index) => {
+            languajeSelected = dbLanguaje.GetLanguaje(index).Name;
+            SetTextsLanguajes();
+        });
+        var langs = dbLanguaje.GetLanguajes();
+        languajeSelect.AddOptions(langs);
+        for(int i = 0; i < langs.Count; i++)
+            if(langs[i] == languajeSelected)
+                languajeSelect.value = i;
+        SetTextsLanguajes();
+    }
+    void SetTextsLanguajes(){
+        foreach(var t in texts)
+            t.textUI.text = dbLanguaje.GetLanguajeContent(languajeSelected, t.indexContent).content;
+    }
+}
---
+++ Assets/MultiLanguaje/Example/script/TextForChange.cs
@@ -0,0 +1,8 @@
+using System;
+using UnityEngine.UI;
+
+[Serializable]
+public class TextForChange {
+    public Text textUI;
+    public int indexContent;
+}
